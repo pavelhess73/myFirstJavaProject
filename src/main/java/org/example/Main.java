e059d12c1ffe037dcdd70ae2fbf5c00af6be7188
Pavel Hess <pavelhess@gmail.com>
2024-06-20 18:15:29 +0200

    dnes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome Java!!!!");
         System.out.println("Hi, Java");
+        System.out.println("6 + 7 = ");
+        System.out.println("6 + 7 = ");
     }
 }
+
